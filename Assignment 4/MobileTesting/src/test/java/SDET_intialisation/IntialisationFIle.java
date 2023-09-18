9782e3a4f66fedb39d9d4f5318fd37aedc083326
AnilKumarBatchu <118332415+AnilKumarBatchu@users.noreply.github.com>
2023-09-18 09:35:38 +0530

    Initial commit

---
+++ Assignment 4/MobileTesting/src/test/java/SDET_intialisation/IntialisationFIle.java
@@ -0,0 +1,16 @@
+package SDET_intialisation;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import SDET_capstone.MobileTesting.Caluculator;
+
+public class IntialisationFIle extends Caluculator{
+	@Test
+public void CaluculatorSDET_Data() {
+//		DesiredCapabilities cals=new DesiredCapabilities();
+//		
+		
+	
+}
+}
